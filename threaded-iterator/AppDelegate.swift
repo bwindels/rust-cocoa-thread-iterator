b5b58b75b56971d159c5b2877a12cae056a5d442
Bruno Windels <bruno.windels@gmail.com>
2015-12-15 14:20:20 +0100

    Initial Commit

---
+++ threaded-iterator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  threaded-iterator
+//
+//  Created by Bruno Windels on 15/12/15.
+//  Copyright © 2015 Bruno Windels. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
